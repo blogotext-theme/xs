f6a23cf3bed482692ec2a016c99a006d8dc43701
kris <kris@xoofoo.org>
2013-11-01 18:23:08 +0100

    Initial files

--- README.md
+++ README.md
@@ -1,4 +1,39 @@
-xs
-==
+# BLOGOTEXT XS
+**Un thème accessible pour Blogotext**
 
-Thème simple, très lisible, qui prend en charge différents formats d’écrans et l’impression.
+## PRÉSENTATION
+Blogotext XS est un thème HTML 5 et CSS 3 pour le cms [Blogotext][blogotext] par Timo.
+
+Ce thème mise sur l’accessibilité (haut niveau de contraste, responsive design, rôles WAI-ARIA) et la sémantique (balisage fort, microdatas, microformats).
+
+## INSTALLATION
+
+0.	Téléchargez l’archive [blogotext-xs.7z][blogotext-xs] ([7zip][dl-7zip] - 6.3 Kio).
+1.	Décompressez l’archive dans le répertoire "themes" de votre Blogotext.
+2.	Rendez-vous sur le panneau d’administration de votre blog.
+3.	Allez dans "Préférences", "Apparence", et choisissez le thème "xs".
+
+Vous pouvez modifier le thème à votre guise.
+
+## HISTORIQUE DES VERSIONS
+
+*	2012-10-16 : version beta.
+*	2012-11-11 : version 1.
+*	2013-01-26 : version 2.
+
+## LICENCE
+Blogotext XS est diffusé sous [licence CC0 1.0][licence-blogotext-xs].  
+Il est actuellement maintenu par [Jonathan Renoult](mailto:renoult.jonathan@tilde3.eu).
+
+### À PROPOS DE CE FICHIER
+
+	Copying and distribution of this file, with or without modification,
+	are permitted in any medium without royalty provided the copyright
+	notice and this notice are preserved. This file is offered as-is,
+	without any warranty.
+
+<!-- LIENS -->
+[blogotext]: <http://lehollandaisvolant.net/blogotext/fr/>
+[blogotext-xs]: <http://renoult-jonathan.tilde3.eu/mes-scripts/blogotext-xs.7z>
+[licence-blogotext-xs]: <http://creativecommons.org/publicdomain/zero/1.0/deed.fr>
+[dl-7zip]: <http://www.spiroo.be/7zip/> "Télécharger le logiciel 7zip"
---
+++ balisage-commentaires.js
@@ -0,0 +1,45 @@
+/* Tag script found at : http://www.siteduzero.com/tutoriel-3-34703-insertion-de-balises-dans-une-zone-de-texte.html */
+function insertTag(startTag, endTag, tag, tagType) {
+	var field = document.getElementById(tag);
+	var scroll = field.scrollTop;
+	field.focus();
+
+	if (window.ActiveXObject) { // C'est IE
+		var textRange = document.selection.createRange();            
+		var currentSelection = textRange.text;
+
+		textRange.text = startTag + currentSelection + endTag;
+		textRange.moveStart("character", -endTag.length - currentSelection.length);
+		textRange.moveEnd("character", -endTag.length);
+		textRange.select();     
+	} else { // c'est pas IE.
+		var startSelection   = field.value.substring(0, field.selectionStart);
+		var currentSelection = field.value.substring(field.selectionStart, field.selectionEnd);
+		var endSelection     = field.value.substring(field.selectionEnd);
+		if (currentSelection == "") { currentSelection = "TEXTE"; }
+		field.value = startSelection + startTag + currentSelection + endTag + endSelection;
+		field.focus();
+		field.setSelectionRange(startSelection.length + startTag.length, startSelection.length + startTag.length + currentSelection.length);
+	}
+	field.scrollTop = scroll;
+}
+
+function reply(code) {
+	var field = document.getElementById('form-commentaire').getElementsByTagName('textarea')[0];
+	field.focus();
+	if (field.value !== '') {
+		field.value += '\n\n';
+	}
+	field.value += code;
+	field.scrollTop = 10000;
+	field.focus();
+}
+
+function resize(id, dht) {
+	var elem = document.getElementById(id);
+	var ht = elem.offsetHeight;
+	size = Number(ht)+Number(dht);
+	elem.style.height = size+"px";
+	return false;
+}
+
